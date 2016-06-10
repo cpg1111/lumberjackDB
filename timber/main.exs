f5633736fbb23674560289a59f011fdf508f5f9a
Christian Grabowski <cpg1111@users.noreply.github.com>
2016-06-09 23:52:24 -0400

    timber conf parsing (#3)

--- timber/main.exs
+++ timber/main.exs
@@ -1,3 +1,8 @@
-defmodule main do
-    
+require Tomlex
+
+defmodule Main do
+    def init() do
+        sharedConf = File.read("../config/shared.toml")
+        timberConf = File.read("../config/timber.toml")
+    end
 end

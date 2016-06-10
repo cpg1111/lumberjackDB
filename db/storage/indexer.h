128b915be6501f34b7bf7c6e214cc9bb27452a29
Christian Grabowski <christiang11754@gmail.com>
2016-06-09 22:24:42 -0400

    Stub out storage

---
+++ db/storage/indexer.h
@@ -0,0 +1,17 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+
+#ifndef INDEXER_H_
+#define INDEXER_H_
+    extern int errno;
+
+    typedef struct {
+        uint id
+        char* collection
+
+    } Indexer
+#endif
